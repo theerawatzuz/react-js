033b43d05a5aaafee340108914d269590fe2b89a
TheerawatNB <bankt21@gmail.com>
2023-12-25 16:32:34 +0700

    Ex/

--- react-1/exercise-3/src/App.jsx
+++ react-1/exercise-3/src/App.jsx
@@ -10,13 +10,32 @@ function App() {
 
 const RegistrationForm = () => {
   return (
-    // Code here
+    <div class="container">
+    <h1>Registration Form</h1>
+    <form id="registrationForm">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  </div>
   );
 };
 
 const DisplayMember = () => {
+  let member1 = "John Doe (john@example.com)"
+  let member2 = "Jane Smith (jane@example.com)"
   return (
-    // Code here
+    <div class="members">
+      <h2>Registered Members</h2>
+      <div class="member">{member1}</div>
+      <div class="member">{member2}</div>
+    </div>
   );
 };
 
